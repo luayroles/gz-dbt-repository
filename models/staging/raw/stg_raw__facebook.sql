becb9afac6f2382101f62dff5a9ef17e4f8be3ba
luciedawagon@gmail.com <luciedawagon@gmail.com>
2025-02-12 13:52:31 +0000

    commit primary key et not null

---
+++ models/staging/raw/stg_raw__facebook.sql
@@ -0,0 +1,29 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'facebook') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        paid_source,
+        CONCAT(date_date,"_",campaign_key) AS primary_key,
+        CASE
+        WHEN campaign_key IS NULL THEN "N/A"
+        END AS campaign_key,
+        CASE
+        WHEN campgn_name IS NULL THEN "N/A"
+        END AS campaign_name,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
+        impression,
+        click
+        CASE
+    from source
+
+)
+
+select * from renamed
